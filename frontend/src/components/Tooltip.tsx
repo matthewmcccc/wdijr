74d7d8553dd8bcb9011dc45d1cd42d1c056c04ac
matthewmcccc <xyzmonte@protonmail.com>
2026-04-13 15:44:36 +0100

    fix(frontend): bump up z-index on tooltip description

--- frontend/src/components/Tooltip.tsx
+++ frontend/src/components/Tooltip.tsx
@@ -13,7 +13,7 @@ const TooltipComponent = ({ content, title }: TooltipProps) => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#228B22" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-info-icon lucide-info"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg>
                 </Tooltip.Trigger>
                 <Tooltip.Portal>
-                    <Tooltip.Content className="border border-gray-300 bg-white text-black p-2 rounded max-w-xs text-sm z-50 whitespace-pre-line">
+                    <Tooltip.Content className="border border-gray-300 bg-white text-black p-2 rounded max-w-xs text-sm z-[9999] whitespace-pre-line">
                         {title && <div className="font-bold text-lg">{title}</div>}
                         {title && <hr className="my-2 border-gray-300"/>}
                         {content}
